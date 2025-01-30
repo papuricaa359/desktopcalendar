c6c800c30326cc2e79f2f8ddf7d5f0ebd0eaa69c
papuricaa359 <papuricaa359@gmail.com>
2025-01-30 17:02:43 +0900

    Delete create directory

--- create/rule.js
+++
@@ -1,7 +0,0 @@
-document.getElementById("agree").addEventListener("click", function () {
-    let rule = document.getElementById("rule");
-    rule.style.display = "none";
-});
-document.getElementById("disagree").addEventListener("click", function () {
-    location.href = "/";
-});
