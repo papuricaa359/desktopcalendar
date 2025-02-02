c918d9f7b7b96193d316922a58cce896eb337074
papuricaa359 <papuricaa359@gmail.com>
2025-02-02 16:21:05 +0900

    Update typechange.js

--- scripts/create/typechange.js
+++ scripts/create/typechange.js
@@ -1,23 +1,30 @@
 import { processImage_type1 } from "./type1.js";
 import { processImage_type2 } from "./type2.js";
 import { processImage_type3 } from "./type3.js";
-
 let selectedType = "1";
-
 document.querySelectorAll('input[name="type"]').forEach((radio) => {
     radio.addEventListener("change", (e) => {
         selectedType = e.target.value;
-        document.querySelectorAll("[id^='imageInput']").forEach((fileInput, index) => {
-            const file = fileInput.files[0];
+        const visibleInput = document.querySelector(".input[style*='display: block;'] input[type='file']");
+        if (visibleInput) {
+            const index = parseInt(visibleInput.id.replace("imageInput", ""), 10);
+            const file = visibleInput.files[0];
             if (file) {
-                const framePath = `/desktopcalendar/frame/2025/type${selectedType}/${index + 1}.png`;
-                const previewId = `imagePreview${index + 1}`;
-                handleFileChange(fileInput, index, file, framePath, previewId);
+                const framePath = `/desktopcalendar/frame/2025/type${selectedType}/${index}.png`;
+                const previewId = `imagePreview${index}`;
+                handleFileChange(visibleInput, index, file, framePath, previewId);
             }
-        });
+        }
     });
 });
-
+document.querySelectorAll("[id^='imageInput']").forEach((fileInput, index) => {
+    fileInput.addEventListener("change", (e) => {
+        const file = fileInput.files[0];
+        const framePath = `/desktopcalendar/frame/2025/type${selectedType}/${index + 1}.png`;
+        const previewId = `imagePreview${index + 1}`;
+        handleFileChange(fileInput, index + 1, file, framePath, previewId);
+    });
+});
 function handleFileChange(fileInput, index, file, framePath, previewId) {
     if (selectedType === "1") {
         processImage_type1(file, framePath, previewId);
@@ -27,12 +34,3 @@ function handleFileChange(fileInput, index, file, framePath, previewId) {
         processImage_type3(file, framePath, previewId);
     }
 }
-
-document.querySelectorAll("[id^='imageInput']").forEach((fileInput, index) => {
-    fileInput.addEventListener("change", (e) => {
-        const file = fileInput.files[0];
-        const framePath = `/desktopcalendar/frame/2025/type${selectedType}/${index + 1}.png`;
-        const previewId = `imagePreview${index + 1}`;
-        handleFileChange(fileInput, index, file, framePath, previewId);
-    });
-});
