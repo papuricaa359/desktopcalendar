467e3aa24832b75ec46f0f576f18822ff3d6f5fe
papuricaa359 <papuricaa359@gmail.com>
2025-02-02 21:55:59 +0900

    Update standtype1.js

--- scripts/create/standtype1.js
+++ scripts/create/standtype1.js
@@ -38,7 +38,7 @@ export async function generateStandImage_type1() {
       }
 
       const textImg = new Image();
-      textImg.src = "/desktopcalendar/frame/stand/text/standtype2text.png";
+      textImg.src = "/desktopcalendar/frame/stand/text/standtype1text.png";
 
       await new Promise((res) => {
         textImg.onload = () => {
