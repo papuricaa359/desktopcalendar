a252fe8e61d995b0ab0d4470f816705e009a8134
papuricaa359 <papuricaa359@gmail.com>
2025-02-09 11:10:06 +0900

    Add files via upload

--- scripts/create/fontselect.js
+++ scripts/create/fontselect.js
@@ -2,8 +2,10 @@ export let fontstype = null;
 document.getElementById("font1").addEventListener("click", function() {
     fontstype = "fonts1";
     document.querySelector(".fontselect").style.display = "none";
+    document.querySelector(".typeselect").style.display = "flex";
 });
 document.getElementById("font2").addEventListener("click", function() {
     fontstype = "fonts2";
     document.querySelector(".fontselect").style.display = "none";
+    document.querySelector(".typeselect").style.display = "flex";
 });
