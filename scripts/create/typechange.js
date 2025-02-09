0d0103502ae590849b94f54b916ae73afeec0d58
papuricaa359 <papuricaa359@gmail.com>
2025-02-09 12:16:31 +0900

    Add files via upload

--- scripts/create/typechange.js
+++ scripts/create/typechange.js
@@ -2,31 +2,25 @@ import { processImage_type1 } from "./type1.js";
 import { processImage_type2 } from "./type2.js";
 import { processImage_type3 } from "./type3.js";
 import { fontstype } from "./fontselect.js";
-
 export let selectedType = 0;
-
 document.getElementById("type1").addEventListener("click", function () {
     selectedType = 1;
     updateImagesForSelectedType();
     document.querySelector(".typeselect").style.display = "none";
 });
-
 document.getElementById("type2").addEventListener("click", function () {
     selectedType = 2;
     updateImagesForSelectedType();
     document.querySelector(".typeselect").style.display = "none";
 });
-
 document.getElementById("type3").addEventListener("click", function () {
     selectedType = 3;
     updateImagesForSelectedType();
     document.querySelector(".typeselect").style.display = "none";
 });
-
 document.querySelector(".opentype").addEventListener("click", function () {
     document.querySelector(".typeselect").style.display = "flex";
 });
-
 document.querySelectorAll("[id^='imageInput']").forEach((fileInput, index) => {
     fileInput.addEventListener("change", (e) => {
         const file = fileInput.files[0];
@@ -37,7 +31,6 @@ document.querySelectorAll("[id^='imageInput']").forEach((fileInput, index) => {
         }
     });
 });
-
 function handleFileChange(fileInput, index, file, framePath, previewId) {
     if (selectedType === 1) {
         processImage_type1(file, framePath, previewId);
@@ -47,7 +40,6 @@ function handleFileChange(fileInput, index, file, framePath, previewId) {
         processImage_type3(file, framePath, previewId);
     }
 }
-
 function updateImagesForSelectedType() {
     document.querySelectorAll("[id^='imageInput']").forEach((fileInput, index) => {
         if (fileInput.closest('.input').style.display !== 'none') {
